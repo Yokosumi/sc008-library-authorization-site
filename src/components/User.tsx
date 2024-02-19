743b7b790b532be04435f25903cc48bb65dbb22f
Selman Ciftci <selman.ciftci.coding@gmail.com>
2024-02-19 11:16:43 +0100

    Refactor User component and add anonymous user creation

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -7,10 +7,10 @@ interface IProps {
 export const User = ({ user }: IProps) => {
 	return (
 		<div className="bg-slate-300 p-3 w-[15rem] rounded">
-			<p className="font-semibold">{user.firstName} {user.lastName}</p>
+			<p className="font-semibold">
+				{user.firstName} {user.lastName}
+			</p>
 			<p>{user.email}</p>
-			<p>{user.address.street}</p>
-			<p>{user.address.zipCode} {user.address.city}</p>
 		</div>
 	);
 };

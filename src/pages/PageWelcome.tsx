cce304189cc706e825ac02489b9ad9d5db5444b4
Selman Ciftci <selman.ciftci.coding@gmail.com>
2024-02-19 13:20:04 +0100

    Update access control in user pages

--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -1,13 +1,16 @@
 import { useContext } from "react";
 import { AppContext } from "../AppContext";
+import * as tools from "../tools";
 
 export const PageWelcome = () => {
-	const { books, users } = useContext(AppContext);
+	const { books, users, currentUser } = useContext(AppContext);
 
 	return (
 		<>
 			<p>There are {books.length} books.</p>
-			<p>There are {users.length} users.</p>
+			{tools.isMemberOfAccessGroup(currentUser, "administrators") && (
+				<p>There are {users.length} users.</p>
+			)}
 		</>
 	);
 };

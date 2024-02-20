959a7d4b1566c08a6851d50ac95fefb95ff8ac1d
Selman Ciftci <selman.ciftci.coding@gmail.com>
2024-02-20 11:13:53 +0100

    Add IFormFields interface and fillProfileFormFieldsWithCurrentUserFields function

--- src/tools.ts
+++ src/tools.ts
@@ -1,4 +1,4 @@
-import { ICurrentUser } from "../src/interfaces";
+import { ICurrentUser, IFormFields } from "../src/interfaces";
 
 export const isMemberOfAccessGroup = (
 	currentUser: ICurrentUser,
@@ -8,4 +8,15 @@ export const isMemberOfAccessGroup = (
 		.split(",")
 		.map((m) => m.trim());
 	return accessGroupItems.includes(accessGroups);
+};
+
+export const fillProfileFormFieldsWithCurrentUserFields = (
+	formFields: IFormFields,
+	currentUser: ICurrentUser
+) => {
+	const names = currentUser.fullName.split(" "); // TODO: refactore to send firstName/lastName from backend
+	formFields.firstName = names[0];
+	formFields.lastName = names[1];
+	formFields.login = currentUser.login;
+	formFields.email = currentUser.email;
 };

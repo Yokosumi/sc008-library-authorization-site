e920312e7f660eddcc2f2e7e3a5bc2f4beb52f79
Selman Ciftci <selman.ciftci.coding@gmail.com>
2024-02-20 09:56:38 +0100

    Update login page and profile page

--- src/pages/PageProfile.tsx
+++ src/pages/PageProfile.tsx
@@ -1,4 +1,8 @@
+import { useContext } from "react";
+import { AppContext } from "../AppContext";
+
 export const PageProfile = () => {
+	const { currentUser } = useContext(AppContext);
 	return (
 		<>
 			<p>profile</p>

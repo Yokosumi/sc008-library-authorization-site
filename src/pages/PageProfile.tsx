ceadff8a0f5c2f7ed47764a1b0db4baed571ebee
Selman Ciftci <selman.ciftci.coding@gmail.com>
2024-02-20 10:06:13 +0100

    Refactor PageProfile component layout

--- src/pages/PageProfile.tsx
+++ src/pages/PageProfile.tsx
@@ -4,10 +4,13 @@ import { AppContext } from "../AppContext";
 export const PageProfile = () => {
 	const { currentUser } = useContext(AppContext);
 	return (
-		<div>
-			<p>Full name:{currentUser.fullName}</p>
-			<p>Login:{currentUser.login}</p>
-			<p>Email:{currentUser.email}</p>
+		<div className="flex gap-3">
+			<img src="images/users/defaultProfile.jpg/" className="w-[7rem]" />
+			<div>
+				<p className="text-2xl">{currentUser.fullName}</p>
+				<p>Login: {currentUser.login}</p>
+				<p>Email: {currentUser.email}</p>
+			</div>
 		</div>
 	);
 };
